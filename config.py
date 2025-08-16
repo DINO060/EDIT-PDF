40758b86f32f4f9c6ec974ec405ef78adc95fc12
DINO060 <johndiverson0@gmail.com>
2025-08-16 17:39:33 -0400

    📈 Updated config_example.py: MAX_FILE_SIZE increased to 1 GB

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-# Configuration du bot Telegram PDF - EXEMPLE
-# Copie ce fichier vers config.py et remplace les valeurs par tes propres clés API
+# Configuration du bot Telegram PDF
+# Remplace les valeurs par tes propres clés API
 
 # Clés Telegram (obtenues sur https://my.telegram.org)
-API_ID = 12345678  # Remplace par ton API_ID
-API_HASH = "abcdef1234567890abcdef1234567890"  # Remplace par ton API_HASH
-BOT_TOKEN = "7638360605:AAHEapDiCSvX-nSwaoKlQxgf0vEujrgAIwY"  # Remplace par ton BOT_TOKEN
+API_ID = 22142337  # Remplace par ton API_ID
+API_HASH = "18eef60818a0a15ab2e8bcaeac66698e"  # Remplace par ton API_HASH
+BOT_TOKEN = "8172549450:AAGs8OlyIe5j7SF_Xm5k_eHnEj5PWKz_-SY"  # Remplace par ton BOT_TOKEN
+ADMIN_IDS = "7570539064,7615697178"  # IDs des admins sous forme de chaîne, séparés par des virgules
 
 # Configuration du bot
 MAX_FILE_SIZE = 1024 * 1024 * 1024  # 1 GB
